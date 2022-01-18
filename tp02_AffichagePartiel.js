4a45c084da5f3926f14957e11b4e5078830756eb
MrTrnr <marietournier.pro@gmail.com>
2022-01-18 12:18:35 +0100

    tp02_AfficahgePartiel ok

---
+++ tp02_AffichagePartiel.js
@@ -0,0 +1,6 @@
+let array = [1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4];
+for (let i = 0; i < array.length; i++) {
+  if (array[i] > 3) {
+    console.log("Entier supérieur à 3 : " + array[i]);
+  }
+}
