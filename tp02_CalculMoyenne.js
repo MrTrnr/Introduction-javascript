885854c46829e1c8324c981315252a2b26873eff
MrTrnr <marietournier.pro@gmail.com>
2022-01-18 12:25:47 +0100

    tp02_CalculMoyenne ok

--- tp02_CalculMoyenne.js
+++ tp02_CalculMoyenne.js
@@ -1,6 +1,6 @@
 let array = [1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4];
+let somme = 0;
 for (let i = 0; i < array.length; i++) {
-  if (array[i] > 3) {
-    console.log("Entier supérieur à 3 : " + array[i]);
-  }
+  somme += array[i];
 }
+console.log("Moyenne des éléments du tableau : " + somme / array.length);
