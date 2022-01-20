6d67e19a0e2de302a3546ab5628e0e71a24a6658
MrTrnr <marietournier.pro@gmail.com>
2022-01-21 00:41:17 +0100

    tp08 tableauPays test min et max ok

--- tp08-tableauPays.js
+++ tp08-tableauPays.js
@@ -1,75 +1,129 @@
-function construireTableau(text) {
-  console.log(text);
-  let contenu = document.querySelector("#contenu");
+function construireTableau(texte) {
+  let contenuTableau = document.querySelector("#contenuTableau");
+  let emplacementTitre = document.querySelector("#contenuTitre");
+  let data = JSON.parse(texte);
 
-  let data = JSON.parse(text);
+  let contenuTitre = `<h2>Pays les plus polluants pour le ${data.polluant} (${data.unite}) en ${data.annee}</h2>`;
+  emplacementTitre.innerHTML = contenuTitre;
 
-  contenuAAjouter = `<table class="table">`;
-  contenuAAjouter += `<caption>Liste des utilisateurs</caption>`;
-  contenuAAjouter += `<thead><tr>
-  <th>id</th>
-  <th>Nom</th>
-  <th>Prénom</th>
-  <th>Date de naissance</th>
-  <th>Lieu de naissance</th>
-  <th>Département</th>
-  <th>Numéro de rue</th>
-  <th>Libellé de rue</th>
-  <th>Code postal</th>
-  <th>Ville</th>
-  </tr></thead>`;
+  let contenuAAjouter = `<table class="table">`;
+  contenuAAjouter += `<thead><tr><th></th><th>Nom</th><th>Valeur</th><th>%</th></tr></thead>`;
   contenuAAjouter += `<tbody>`;
-  for (let i = 0; i < data.length; i++) {
-    contenuAAjouter += `<tr>
-    <th>${data[i].id}</th>
-    <th>${data[i].nom}</th>
-    <th>${data[i].prenom}</th>
-    <th>${data[i].dateNaissance}</th>
-    <th>${data[i].lieuNaissance}</th>
-    <th>${data[i].departementNaissance}</th>
-    <th>${data[i].numeroRue}</th>
-    <th>${data[i].libelleRue}</th>
-    <th>${data[i].codePostal}</th>
-    <th>${data[i].ville}</th>
-    </tr>`;
+
+  for (let i = 0; i < data.pays.length; i++) {
+    contenuAAjouter += `<tr><td><img src="https://flagcdn.com/32x24/${data.pays[i].code}.png"></td>
+    <td>${data.pays[i].nom}</td><td>${data.pays[i].valeur}</td><td>${data.pays[i].pourcentage}</td></tr>`;
   }
   contenuAAjouter += `</tbody></table>`;
-  contenu.innerHTML = contenuAAjouter;
+  contenuTableau.innerHTML = contenuAAjouter;
 }
 
-function init() {
-  fetch("https://digicode.cleverapps.io/utilisateurs")
-    .then(function (response) {
-      if (response.status != 200) {
+function afficherDonnees() {
+  fetch("https://digicode.cleverapps.io/json/pays/pollution")
+    .then(function (reponse) {
+      if (reponse.status != 200) {
         console.log("La requête n'a pas abouti");
       }
-      return response.text();
+      return reponse.text();
     })
-    .then(function (text) {
-      construireTableau(text);
+    .then(function (texte) {
+      construireTableau(texte);
     });
 }
 
-function envoyer() {
-  let objet = {};
-  objet.nom = document.querySelector("#nom").value;
-  objet.prenom = document.querySelector("#prenom").value;
-  objet.dateNaissance = document.querySelector("#dateNaissance").value;
-  objet.lieuNaissance = document.querySelector("#lieuNaissance").value;
-  objet.departementNaissance = document.querySelector(
-    "#departementNaissance"
-  ).value;
-  objet.numeroRue = document.querySelector("#numeroRue").value;
-  objet.libelleRue = document.querySelector("#libelleRue").value;
-  objet.codePostal = document.querySelector("#codePostal").value;
-  objet.ville = document.querySelector("#ville").value;
+function selectionNombre(nomChamp) {
+  console.log("appel fonction selectionNombre");
+  let saisie = document.querySelector("input[name='" + nomChamp + "']").value;
+  console.log("saisie : " + saisie + "nom champ : " + nomChamp);
+  try {
+    //la saisie contient autre chose que des nombres
+    if (isNaN(saisie) && saisie != "") {
+      throw "vous devez saisir un nombre";
+      //la saisie n'est pas vide et contient des nombres
+    } else if (saisie != "") {
+      return parseFloat(saisie);
+      //la saisie ne contient rien et concerne le champ minimum
+    } else if (nomChamp == "minimum") {
+      return 0;
+    }
+    //par déduction la saisie ne contient rien et concerne le champ maximum
+    else {
+      return Number.MAX_VALUE;
+    }
+  } catch (error) {
+    let element = document.querySelector("#affichageErreur");
+    element.innerHTML =
+      "<span style='background-color:red'>" + error + "</span>";
+  }
+}
 
-  fetch("https://digicode.cleverapps.io/utilisateurs", {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json;charset=UTF-8",
-    },
-    body: JSON.stringify(objet),
-  }).then((response) => response.text().then(() => init()));
+function traiterNombres() {
+  console.log("appel fonction traiterNombres");
+  let premierNombre = selectionNombre("minimum");
+  let deuxiemeNombre = selectionNombre("maximum");
+  try {
+    if (premierNombre > deuxiemeNombre) {
+      throw "vous devez saisir un nombre minimimum inférieur à nombre maximum";
+    } else if (premierNombre < 0) {
+      throw "vous devez saisir un nombre minimimum positif";
+    } else if (deuxiemeNombre < 0) {
+      throw "vous devez saisir un nombre maximum positif";
+    } else {
+      console.log("premier nombre : " + premierNombre);
+      console.log("deuxième nombre : " + deuxiemeNombre);
+    }
+  } catch (error) {
+    let element = document.querySelector("#affichageErreur");
+    element.innerHTML =
+      "<span style='background-color:red'>" + error + "</span>";
+  }
+}
+
+function effacerMessageErreur() {
+  let element = document.querySelector("#affichageErreur");
+  element.innerHTML = "<span></span>";
+  console.log("appel fonction valider");
+}
+
+function valider() {
+  effacerMessageErreur();
+  traiterNombres();
+
+  //vérifier le champ min
+  //vérifier le champ max
+  //récupérer les données
+  //n'afficher que les données correspondantes
+}
+
+function construireTableauMinMax(texte) {
+  let contenuTableau = document.querySelector("#contenuTableau");
+  let emplacementTitre = document.querySelector("#contenuTitre");
+  let data = JSON.parse(texte);
+
+  let contenuTitre = `<h2>Pays les plus polluants pour le ${data.polluant} (${data.unite}) en ${data.annee}</h2>`;
+  emplacementTitre.innerHTML = contenuTitre;
+
+  let contenuAAjouter = `<table class="table">`;
+  contenuAAjouter += `<thead><tr><th></th><th>Nom</th><th>Valeur</th><th>%</th></tr></thead>`;
+  contenuAAjouter += `<tbody>`;
+
+  for (let i = 0; i < data.pays.length; i++) {
+    contenuAAjouter += `<tr><td><img src="https://flagcdn.com/32x24/${data.pays[i].code}.png"></td>
+    <td>${data.pays[i].nom}</td><td>${data.pays[i].valeur}</td><td>${data.pays[i].pourcentage}</td></tr>`;
+  }
+  contenuAAjouter += `</tbody></table>`;
+  contenuTableau.innerHTML = contenuAAjouter;
+}
+
+function afficherDonneesMinMAx() {
+  fetch("https://digicode.cleverapps.io/json/pays/pollution")
+    .then(function (reponse) {
+      if (reponse.status != 200) {
+        console.log("La requête n'a pas abouti");
+      }
+      return reponse.text();
+    })
+    .then(function (texte) {
+      construireTableauMinMax(texte);
+    });
 }
