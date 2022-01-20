06edf72b2e55ad072d433f55eb8b453e21e51a3a
MrTrnr <marietournier.pro@gmail.com>
2022-01-20 14:57:57 +0100

    tp06 affichagePaysEtCapitale ok

--- tp06-affichageDonnees.js
+++ tp06-affichageDonnees.js
@@ -1,25 +1,26 @@
-function construireContenu(donnees) {
-  console.log("fonction condtuire contenu");
+function construireContenu(texte) {
+  console.log("fonction construire contenu");
 
   let contenu = document.querySelector("#contenu");
+  let donnees = JSON.parse(texte);
+  let contenuAAficher = "";
+  for (let i = 0; i < donnees.length; i++) {
+    contenuAAficher += `Pays : ${donnees[i].name.common} Capitale : ${donnees[i].capital} Population : ${donnees[i].population} Continent : ${donnees[i].continents}<br>`;
+  }
 
-  contenu.innerHTML = `${donnees}`;
+  contenu.innerHTML = contenuAAficher;
 }
 
 function afficherContenu() {
   fetch("https://restcountries.com/v3.1/all")
     .then(function (reponse) {
       if (reponse.status != 200) {
-        throw "La requête n'a pas abouti";
+        console.log("La requête n'a pas abouti");
       }
-
       return reponse.text();
     })
-    .then(function (donnees) {
-      console.log(donnees);
-      construireContenu(donnees);
-    })
-    .catch(function (erreur) {
-      console.log(erreur);
+    .then(function (texte) {
+      console.log(texte);
+      construireContenu(texte);
     });
 }
